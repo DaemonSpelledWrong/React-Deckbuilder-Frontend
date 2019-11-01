ad5c504744d6ac978e848b2a3d3e1bbf3cb83c91
Damon Self <damongself@gmail.com>
2019-11-01 13:43:18 -0600

    Reverts CardContainer to class component and adds pagination - Forgot I had changed CardContainer to a class component to prepare for adding in pagination features and have now reverted it back to a class component. - Added in basic functioning pagination by page number. Styled poorly currently. - In the future, will implement arrow buttons that increment and decrement [i] manually in order to change pages rather than by page number.

--- src/components/CardContainer/index.js
+++ src/components/CardContainer/index.js
@@ -1,22 +1,81 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Card from '../Card'
 import './main.css'
 
+export default class CardContainer extends Component {
+  state = {
+    currentPage: 1,
+    cardsPerPage: 21
+  }
 
-export default ({allCards}) => {
-  const cardRender = allCards.map(card => (
-      <Card 
-        key={card.name}
-        name={card.name}
-        image={card.image}
-      />
-  ))
+  handleClick = event => {
+    this.setState({
+      currentPage: Number(event.target.id)
+    })
+  }
+  
+  render() {
+    this.handleClick = this.handleClick.bind(this);
+    const { currentPage, cardsPerPage } = this.state;
+    const allCards = this.props.allCards
 
-  return(
-    <section className="container-zone">
-      <section className="card-container">
-        {cardRender}
+    const indexOfLastPage = currentPage * cardsPerPage;
+    const indexOfFirstPage = indexOfLastPage - cardsPerPage;
+    const currentCards = allCards.slice(indexOfFirstPage, indexOfLastPage);
+
+    const renderCards = currentCards.map((card, index) => {
+      return(
+        <Card 
+          key={card.name}
+          name={card.name}
+          image={card.image}
+        />
+      )
+    })
+
+    const pageNumbers = [];
+    for (let i = 1; i <= Math.ceil(allCards.length / cardsPerPage); i++) {
+      pageNumbers.push(i)
+    }
+
+    const renderPageNumbers = pageNumbers.map(number => {
+      return (
+        <li
+          key={number}
+          id={number}
+          onClick={this.handleClick}
+        >
+          {number}
+        </li>
+      );
+    })
+    return(
+      <section className="container-zone">
+        <section className="card-container">
+          {renderCards}
+        </section>
+        <ul id='page-numbers'>
+          {renderPageNumbers}
+        </ul>
       </section>
-    </section>
-  )
+    )
+  }
 }
+
+// export default ({allCards}) => {
+//   const cardRender = allCards.map(card => (
+//       <Card 
+//         key={card.name}
+//         name={card.name}
+//         image={card.image}
+//       />
+//   ))
+
+//   return(
+//     <section className="container-zone">
+//       <section className="card-container">
+//         {cardRender}
+//       </section>
+//     </section>
+//   )
+// }
