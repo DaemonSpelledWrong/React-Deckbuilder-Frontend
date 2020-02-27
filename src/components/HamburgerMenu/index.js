54218a48ad6dedf812f0c939c1b9b810d5fcd449
Damon Self <damongself@gmail.com>
2020-02-26 22:27:05 -0500

    Adds event listener to HamburgerMenu component
    Now whenever the menu button is clicked it logs "Hit" to the console.
    In the future this will be used to display the navigation options a
    user can traverse.

--- src/components/HamburgerMenu/index.js
+++ src/components/HamburgerMenu/index.js
@@ -4,10 +4,14 @@ import React from 'react';
 // } from "react-router-dom";
 import './main.css'
 
+function testMenu() {
+  console.log('Hit');
+}
+
 const HamburgerMenu = () => {
   return(
     <div>
-      <i className="fa fa-bars"></i>
+      <i className="fa fa-bars" onClick={testMenu}></i>
     </div>
   )
 }
