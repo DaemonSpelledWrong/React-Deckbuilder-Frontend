efa59ba55de0719ec1aa4fb2edf3c775924a30eb
Damon Self <damongself@gmail.com>
2020-03-11 21:37:12 -0400

    Moves state and methods from App component to the DeckBuilder component
    The selectedCards state inside the App component was only used by the
    Deckbuilder component. Therefore, I decided to move that state and any
    associated methods to the Deckbuilder component as it is the top-most
    relevant component.
    
    The editDeck method inside the App component, however, is now broken as
    it would set the state of the selectedCards whenever a user selected a
    specific deck. Now I will work on a solution for that problem that will
    accomodate this change. Or, if the change seems irrational later on, I
    will revert it.

--- src/components/DeckBuilder/index.js
+++ src/components/DeckBuilder/index.js
@@ -1,9 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './main.css'
 
 import CardContainer from '../CardContainer';
 
-export default ({allCards, selectedCards, user, addCard, removeCard, saveNewDeck}) => {
+export default ({allCards, user, deck_id}) => {
+
+  const [ selectedCards, setSelectedCards ] = useState([]);
+
+  const addCard = card => {
+    console.log(selectedCards);
+    return (
+      selectedCards.includes(card)
+    ? null
+    : setSelectedCards([...selectedCards, card])
+    );
+  };
+
+  const removeCard = card => {
+    return selectedCards.includes(card)
+    ? setSelectedCards(selectedCards.filter(thisCard => {
+        return thisCard !== card;
+      }))
+    : null
+  };
+
+  const saveDeck = () => {
+    deck_id !== null
+    ? 
+      fetch(`http://localhost:3000/decks/${deck_id}`, {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          name: 'Deck',
+          user_id: user.data.attributes.id,
+          cards: selectedCards
+        })
+      })
+    :
+      fetch('https://safe-bayou-71328.herokuapp.com/decks', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          user_id: user.data.attributes.id,
+          name: 'Deck',
+          cards: selectedCards
+        })
+      })
+  };
 
   return(
     <section className='deck-zone'>
@@ -17,7 +64,7 @@ export default ({allCards, selectedCards, user, addCard, removeCard, saveNewDeck
         method={removeCard}
         user={user}
         deck = 'deck'
-        saveNewDeck={saveNewDeck}
+        saveNewDeck={saveDeck}
         cardPageCount={9}
       />
     </section>
